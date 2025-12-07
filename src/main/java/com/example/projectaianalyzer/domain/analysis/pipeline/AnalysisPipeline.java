adb851a2b1c3dbd78845507e057defcc6f53121b
hr79 <doox2o4114@gmail.com>
2025-12-08 06:05:48 +0900

    feat: 분석이 모두 완료된 후 클론한 리포지토리 삭제

--- src/main/java/com/example/projectaianalyzer/domain/analysis/pipeline/AnalysisPipeline.java
+++ src/main/java/com/example/projectaianalyzer/domain/analysis/pipeline/AnalysisPipeline.java
@@ -8,6 +8,7 @@ import com.example.projectaianalyzer.domain.analysis.dto.FinalAnalysisDto;
 import com.example.projectaianalyzer.domain.analysis.pipeline.steps.GitCloneStep;
 import com.example.projectaianalyzer.domain.analysis.pipeline.steps.ScanProjectStep;
 import com.example.projectaianalyzer.domain.project.service.FileScannerResult;
+import com.example.projectaianalyzer.infra.util.FileStorage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -21,18 +22,21 @@ public class AnalysisPipeline {
     private final ScanProjectStep scanProjectStep;
     private final AnalyzeFileStructureStep analyzeFileStructureStep;
     private final DomainAnalysisStep domainAnalysisStep;
+    private final FileStorage fileStorage;
 
     public FinalAnalysisDto run(String repoUrl) {
         String path = gitCloneStep.execute(repoUrl);
         FileScannerResult fileScannerResult = scanProjectStep.execute(path);
         List<String> fileStructureSummaries = fileScannerResult.fileStructureSummaries();
         List<FileStructureAnalysisDto> fileStructureAnalysisResults = analyzeFileStructureStep.execute(fileStructureSummaries);
-
-        return domainAnalysisStep.execute(new DomainAnalysisInput(
+        FinalAnalysisDto finalAnalysisDto = domainAnalysisStep.execute(new DomainAnalysisInput(
                         fileStructureAnalysisResults,
                         fileScannerResult.fileInfoList(),
                         path
                 )
         );
+        fileStorage.delete(path);
+
+        return finalAnalysisDto;
     }
 }
